f9f797413d662fe6d9c455e50b1805ae248c06fb
dehavilland95 <fanshakhtar95@gmail.com>
2024-10-12 22:24:39 +0300

    first

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,27 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.models.Car;
+import web.service.CarService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/cars")
+public class CarController {
+
+    @Autowired
+    private CarService carService;
+
+    @GetMapping()
+    public String getCars(@RequestParam(defaultValue = "-1") int count, Model model) {
+        List<Car> cars = carService.getCars(count);
+        model.addAttribute("cars", cars);
+        return "cars";
+    }
+}
---
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -0,0 +1,28 @@
+package web.dao;
+
+import org.springframework.stereotype.Component;
+import web.models.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CarDaoImpl implements CarDao {
+    List<Car> cars;
+    {
+        cars = new ArrayList<Car>();
+        cars.add(new Car("BMW", "red", 200));
+        cars.add(new Car("Mazda", "brown", 220));
+        cars.add(new Car("Kia", "black", 210));
+        cars.add(new Car("Renault", "white", 250));
+        cars.add(new Car("Toyota", "blue", 280));
+    }
+    @Override
+    public List<Car> getCars(int count) {
+        if(count < 0 || count >= 5){
+            return cars;
+        }else{
+            return cars.subList(0, count);
+        }
+    }
+}
