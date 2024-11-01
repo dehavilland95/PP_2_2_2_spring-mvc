76ff229dd6c148c830cd4585362874cce50c2818
dehavilland95 <fanshakhtar95@gmail.com>
2024-11-01 03:21:10 +0300

    fix 4

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,7 +1,6 @@
 package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import web.dao.CarDao;
 import web.models.Car;
